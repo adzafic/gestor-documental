7de9abd567ce78616e54df8fa72c4270f7129584
adzafic <adnan.dzafic21@gmail.com>
2018-03-04 19:30:41 +0100

    getfile service, cambiar color del las carpetas/archivos

--- src/app/components/file/file.component.ts
+++ src/app/components/file/file.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router,ActivatedRoute, Params } from '@angular/router';
+import { FilesService } from "../../services/files.service";
 
 @Component({
   selector: 'app-file',
@@ -7,14 +8,18 @@ import { Router,ActivatedRoute, Params } from '@angular/router';
   styleUrls: ['./file.component.css']
 })
 export class FileComponent implements OnInit {
-  id: string;
+  id: number;
+  file: any;
   constructor(
     private router:Router,
-    private route:ActivatedRoute
+    private route:ActivatedRoute,
+    private filesService:FilesService
   ) { }
 
   ngOnInit() {
     this.id = this.route.snapshot.params['id'];
+    this.file = this.filesService.getfile(this.id);
+    console.log(this.file);
   }
 
 }
--- src/app/services/files.service.ts
+++ src/app/services/files.service.ts
@@ -4,54 +4,63 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class FilesService {
   //files: Observable<any[]>;
-  files: Array<any>;
+  files: Array<any> = [
+    {
+      "name":"DOCUMENTOS CI" ,
+      "tipo":"folder",
+      "ruta":"1"
+    },
+    {
+      "name":"INFORMES ANALITICOS" ,
+      "tipo":"folder",
+      "ruta":"2"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },{
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+    {
+      "name":"CI-123456" ,
+      "tipo":"file"
+    },
+  ];
+  file:any;
   constructor() { }
 
   getFiles(){
-    this.files = [
-      {
-        "name":"DOCUMENTOS CI" ,
-        "tipo":"folder",
-        "ruta":"1"
-      },
-      {
-        "name":"INFORMES ANALITICOS" ,
-        "tipo":"folder",
-        "ruta":"2"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },{
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-      {
-        "name":"CI-123456" ,
-        "tipo":"file"
-      },
-    ];
     return this.files;
+  }
+
+  getfile(id:number){
+    this.file = this.files.filter(function(obj) {
+      //console.log(obj);
+      return obj.ruta == id;
+    });
+
+    return this.file[0];
   }
 }
